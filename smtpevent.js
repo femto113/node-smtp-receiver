6e7f294b72c6f5280e3d7fc910d29bc5e260471c
Ken <ken.woodruff@gmail.com>
2012-02-02 16:24:31 -0800

    interm checkin before final transformation to pure event dispatch

--- smtpevent.js
+++ smtpevent.js
@@ -20,22 +20,31 @@
  * @version 0.0.2
  */
 
-var net = require('net'),
-    util = require('util');
+var net = require('net'), events = require('events');
     
-function SMTPServer(hostname) {
+function SMTPServer(hostname, opts) {
     net.Server.call(this);
-    var that = this;
-    util.log('SMTP server started on "'+ hostname + '"');
 
-    this.on('connection', function (socket) {
-        new SMTPConnection(hostname, that, socket);
-    });
-    
-    this.version = '0.0.2';
-    
+    this.hostname = hostname || require('os').hostname();
+    this.name = opts && opts.name || 'node.js smtpevent server';
+    this.version = opts && opts.version || '0.0.2';
+    this.log = opts && opts.log || require('util').log.bind(this); // TODO: accept as arg
+
+    // connection will call this method when a complete message is received
+    // TODO should pass this as arg to connection?
+    this.incoming = function (remoteAddress, mailfrom, rcpttos, data) {
+      this.emit('incoming-mail', remoteAddress, mailfrom, rcpttos, data);
+    }.bind(this);
+    // TODO don't we want other hooks? like validating recipients?
+   
+    this.on('connection', (function (socket) {
+        this.log('New SMTP connection from: ' + socket.remoteAddress);
+        new SMTPConnection(this, socket);
+    }).bind(this));
+
+    this.log('SMTP server started on "'+ this.hostname + '"');
 };
-util.inherits(SMTPServer, net.Server);
+require('util').inherits(SMTPServer, net.Server);
 
 var SMTPProtocol = {
   verbs: {
@@ -80,141 +89,52 @@ function get_address(keyword, argument) {
     return address;
 }
     
-function SMTPConnection(hostname, server, socket) {
-    util.log('New SMTP connection from: ' + socket.remoteAddress);
+function SMTPConnection(serverArg, socketArg) {
+
+    events.EventEmitter.call(this);
 
     // Private variables to this instance
-    var self = this,
-        hostname = hostname || 'localhost',
-        greeting = 0,
-        mailfrom = null,
-        rcpttos = [];
-        
-    /**
-     * Emit a response to the client
-     * @param {Number} code
-     * @param {String} message
-     */
-    var respond = function (code, message) { socket.write("" + code + " " + message + SMTPProtocol.EOL); }
-    // some sugar for common responses
-    respond.Ok = function () { respond(250, "Ok"); }
-    respond.Syntax = function (verb) { respond(501, "Syntax: " + verb + (SMTPProtocol.verbs[verb] ? " " + SMTPProtocol.verbs[verb] : "")); };
-    
-    /**
-     * Functions to handle incoming SMTP verbs
-     */
-    var SMTP = {
-        HELO: function (argument) {
-            if (greeting) {
-                respond(503, 'Duplicate HELO/EHLO');
-            } else {
-                greeting = argument;
-                respond(250, hostname + ' Hello ' + socket.remoteAddress);
-            }
-            socket.once('data', onVerb);
-        },
-        NOOP: function () {
-            respond.Ok();
-            socket.once('data', onVerb);
-        },
-        QUIT: function () {
-            respond(221, hostname + ' closing connection');
-            socket.end();
-        },
-        MAIL: function (argument) {
-            var address = get_address('FROM:', argument);
-            util.log('===> MAIL ' + argument);
-            
-            if (!address) {
-                respond.Syntax('MAIL');
-                return;
-            }
-            if (mailfrom) {
-                respond(503, 'Error: nested MAIL command');
-                return;
-            }
-            mailfrom = address;
-            util.log('sender: ' + mailfrom);
 
-            respond.Ok();
-            socket.once('data', onVerb);
-        },
-        RCPT: function (argument) {
-            util.log('===> RCPT ' + argument);
-            if (!mailfrom) {
-                respond(503, 'Error: need MAIL command');
-                return;
-            }
-            address = get_address('TO:', argument);
-            if (!address) {
-                respond.Syntax('RCPT');
-                return;
-            }
-            rcpttos.push(address);
-            util.log('recips: ' + rcpttos.join(', '));
-            respond.Ok();
-            socket.once('data', onVerb);
-        },
-        RSET: function () {
-            // Reset the sender, recipients, and data, but not the greeting
-            mailfrom = null;
-            rcpttos = [];
-            respond.Ok();
-            socket.once('data', onVerb);
-        },
-        DATA: function () {
-            if (!rcpttos.length) {
-                respond(503, 'Error: need RCPT command');
-                return;
-            }
-            respond(354, 'End data with <CR><LF>.<CR><LF>');
-            socket.once('data', onData);
-        }
-    }
+    this.server = serverArg;
+    this.socket = socketArg;
+
+    // state variables
+    this.greeting = null;
+//    this.envelope = {};
+    this.mailfrom = null
+    this.rcpttos = []; // TODO move into envelope
+    
+    // bind the prototype handlers to this instance
+    this.SMTP = {};
+    for (verb in this.handlers) this.SMTP[verb] = this.handlers[verb].bind(this);
         
     /**
      * Handle the situation where the client is issuing SMTP commands
      */
-    var onVerb = function (buffer) {
-        var line = buffer.toString();
+    this.onVerb = function (buffer) {
+        // NOTE: toString should be fast enough for sync op because verb lines are always? short
+        var matches = buffer.toString().match(this.reverb);
+        if (!matches) return this.respond(500, 'Error: bad syntax');
 
-        if (!line) {
-            respond(500, 'Error: bad syntax');
-            return;
-        }
-        
-        // TODO: capture via regex
-        var command, argument;
-        var first_space_position = line.indexOf(' ');
-        if (first_space_position < 0) {
-            command = strip(line.toUpperCase());
-            argument = null;
-        } else {
-            command = line.substr(0, first_space_position).toUpperCase();
-            argument = strip(line.substr(first_space_position));
-        }
+        var command = matches[1].toUpperCase(), argument = matches[2];
+console.log("onVerb", "command", command, "argument", argument);
 
-        if (!(command in SMTPProtocol.verbs)) {
-            socket.once('data', onVerb); //  state = COMMAND;
-            respond(502, 'Error: command "' + command + '" not implemented');
-            return;
-        } else {
-          // validate given argument vs expected
-          expected = SMTPProtocol.verbs[command];
-          if (typeof(expected) != "undefined" && !!expected != !!argument) {
-            socket.once('data', onVerb); //  state = COMMAND;
-            respond.Syntax(command);
-          } else {
-            SMTP[command](argument);
-          }
-        }
-    };
+        // see if this is a command we can handle
+        if (!(command in this.SMTP)) return this.respond(502, 'Error: command "' + command + '" not implemented');
+
+        // validate presence of argument if expected, or lack thereof if expected, or ignorability thereof...
+        var expected = SMTPProtocol.verbs[command];
+        if (typeof(expected) != "undefined" && !!expected != !!argument) return this.respondSyntax(command);
+
+console.log("onVerb", "invoking command");
+        return this.SMTP[command](argument);
+    }.bind(this);
 
    /**
     * Handle the case where the client is transmitting data (i.e. not a command)
     */
-    var onData = function (buffer) {
-      var line = buffer.toString();
+    this.onData = function (buffer) {
+      var line = buffer.toString(); // TODO: seems like this won't work well with big messages that need chunking?
 
       var current_data = [], NEWLINE = '\n';
 
@@ -234,30 +154,117 @@ function SMTPConnection(hostname, server, socket) {
           }
       }
       
-      server.emit('incoming-mail',
-                  socket.remoteAddress, mailfrom, rcpttos,
-                  current_data.join(NEWLINE)
-                  );
+      // called the incoming mail callback
+      this.server.incoming(this.socket.remoteAddress, this.mailfrom, this.rcpttos, current_data.join(NEWLINE));
       
-      rcpttos = [];
-      mailfrom = null;
-      respond.Ok();
-      socket.once('data', onVerb); //  state = COMMAND;
-    }
+      // reset envelope (TODO: is this really correct behavior? shouldn't it get preserved until overwritten or an RSET is sent?)
+      this.rcpttos = [];
+      this.mailfrom = null;
+
+      this.respondOk();
+      this.socket.on('data', this.onVerb); // start listening for verbs again
+    }.bind(this);
 
     // Event listeners:
-    socket.on('connect', function () {
-        util.log('Socket connected from: ' + socket.remoteAddress + '. Sending welcome message.');
-        respond(220, hostname +' node.js smtpevent server ' + server.version);
-        socket.once('data', onVerb);
-    });
+    this.socket.on('connect', function () {
+        this.server.log('Socket connected from: ' + this.socket.remoteAddress + '. Sending welcome message.');
+        this.respondWelcome();
+        this.socket.on('data', this.onVerb); // start listening for verbs
+    }.bind(this));
     
-    socket.on('close', function () {
-        util.log('Socket closed, destroying SMTPConnection instance');
-        delete self;
-    });
+    this.socket.on('close', function () {
+        this.server.log('Socket closed, destroying SMTPConnection instance');
+        delete this;
+    }.bind(this));
 }
 
+require('util').inherits(SMTPConnection, events.EventEmitter);
+
+
+// regex for matching an incoming verb
+SMTPConnection.prototype.reverb  = new RegExp("^(?:\s*)([A-Za-z]{4}) ?(.*)(?:\s|\r|\n)*$");
+
+/**
+ * Emit a response to the client
+ * @param {Number} code
+ * @param {String} message
+ */
+SMTPConnection.prototype.respond = function (code, message) {
+console.log("responding", code, message);
+    this.socket.write("" + code + " " + message + SMTPProtocol.EOL);
+}
+// add some sugar for common responses
+SMTPConnection.prototype.respondOk = function () { this.respond(250, "Ok"); }
+SMTPConnection.prototype.respondSyntax = function (verb) { this.respond(501, "Syntax: " + verb + (SMTPProtocol.verbs[verb] ? " " + SMTPProtocol.verbs[verb] : "")); };
+SMTPConnection.prototype.respondWelcome = function () { this.respond(220, [this.server.hostname, this.server.name, this.server.version].join(' ')); };
+
+/**
+ * Functions to handle incoming SMTP verbs
+ */
+SMTPConnection.prototype.handlers = {
+    HELO: function (argument) {
+        if (this.greeting) {
+            this.respond(503, 'Duplicate HELO/EHLO');
+        } else {
+            this.greeting = argument;
+            this.respond(250, this.server.hostname + ' Hello ' + this.socket.remoteAddress);
+        }
+    },
+    NOOP: function () {
+        this.respondOk();
+    },
+    QUIT: function () {
+        this.respond(221, this.server.hostname + ' closing connection');
+        this.socket.end();
+    },
+    MAIL: function (argument) {
+        this.server.log('===> MAIL ' + argument);
+        var address = get_address('FROM:', argument);
+        
+        if (!address) {
+            this.respondSyntax("MAIL");
+            return;
+        }
+        if (this.mailfrom) {
+            this.respond(503, 'Error: nested MAIL command');
+            return;
+        }
+        this.mailfrom = address;
+        this.server.log('sender: ' + this.mailfrom);
+
+        this.respondOk();
+    },
+    RCPT: function (argument) {
+        this.server.log('===> RCPT ' + argument);
+        if (!this.mailfrom) {
+            this.respond(503, 'Error: need MAIL command');
+            return;
+        }
+        var address = get_address('TO:', argument);
+        if (!address) {
+            this.respondSyntax('RCPT');
+            return;
+        }
+        this.rcpttos.push(address);
+        this.server.log('recips: ' + this.rcpttos.join(', '));
+        this.respondOk();
+    },
+    RSET: function () {
+        // Reset the sender, recipients, and data, but not the greeting
+        this.mailfrom = null;
+        this.rcpttos = [];
+        this.respondOk();
+    },
+    DATA: function () {
+        if (!this.rcpttos.length) {
+            this.respond(503, 'Error: need RCPT command');
+            return;
+        }
+        this.respond(354, 'End data with <CR><LF>.<CR><LF>');
+        this.socket.removeAllListeners('data'); // stop listening for verbs
+        this.socket.once('data', this.onData); // start listening for data
+    }
+}
 
 // Export public API:
 exports.SMTPServer = SMTPServer;
