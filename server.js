d5262d794f9d7fbd57b92d1b291a92b3f89f5972
Ken Woodruff <ken.woodruff@gmail.com>
2019-06-13 17:31:18 -0700

    set default port to the semi-standard 2525, use createServer instead of constructor

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
-var SMTPServer = require("./smtpevent.js").SMTPServer;
+// This is a minimal server example, runs on port 2525 and prints messages to stdout, logs to stderr
+var smtp = require(".");
 
-var server = new SMTPServer('localhost');
-server.listen(1025, "127.0.0.1");
-server.on('incoming-mail', console.log.bind(console));
-
+var server = smtp.createServer(
+  'smtp.example.com',                   // hostname
+  { log: console.error.bind(console) }, // options
+  console.log.bind(console)             // "message" event listener
+);
+server.listen(2525, "127.0.0.1"); // change address to 0.0.0.0 to listen on external interfaces
