e2fdd3d663087c663def2bc63e1eda81e51a23ba
Ken Woodruff <ken.woodruff@gmail.com>
2019-06-17 11:47:38 -0700

    fix PORT and read addrs from mailboxes.json

--- tests/starttls.py
+++ tests/starttls.py
@@ -2,23 +2,22 @@
 # -*- coding: utf-8 -*-
 
 from smtplib import SMTP
-from telnetlib import Telnet
 import unittest
+
+PORT = 2525
 
 class SequentialTests(unittest.TestCase):
     
-    addrs = [
-        'bob@example.com', 'sheila@example.com', 'kurt@example.com',
-        'wendy@example.com', 'tim@example.com'
-        ]
+    def setUp(self):
+        self.smtp = SMTP('localhost', PORT)
+        with open('mailboxes.json', 'r') as f:
+            self.mailboxes = json.load(f)
+        # Turning this on will echo the full conversation, useful for debugging
+        # self.smtp.set_debuglevel(True)
         
     def testStarttls(self):
         """starttls is supported"""
         
-        smtp = SMTP('localhost', 1025)
-        # smtp = SMTP('ec2-18-208-163-245.compute-1.amazonaws.com', 25)
-        smtp.set_debuglevel(True) # turns on client side debug messages
-
         code, text = smtp.ehlo()
         self.assertTrue(smtp.does_esmtp, msg="after EHLO does_estmp should be truish")
         self.assertIn('starttls', smtp.esmtp_features, msg="after EHLO 'starttls' should be in esmtp_features")
@@ -30,11 +29,9 @@ class SequentialTests(unittest.TestCase):
         self.assertEqual(code, 250, msg="one additional EHLO after STARTTLS should not cause an error")
         self.assertNotIn('starttls', smtp.esmtp_features, msg="after STARTTLS + EHLO 'starttls' should no longer be in esmtp_features")
 
-        response = smtp.sendmail(
-            'alice@example.com',
-            'bob@example.com',
-            'This is a test message\nSecond line.\nFinal line here.'
-        )
+        # send a mail message to ensure the upgraded connection works
+        addrs = Object.keys(self.mailboxes)
+        response = smtp.sendmail(addrs[0], addrs[1], 'This is a test message\nSecond line.\nFinal line here.')
         self.assertEqual(response, {})
         smtp.quit()
 
